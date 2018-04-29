c3381430079a9ecd75ac1b2a3a23328a9231ac6a
aleksanderkol00 <a.kolowski@student.uksw.edu.pl>
2018-04-29 13:58:49 +0200

    Add sum grey picture method.

--- src/pl/uksw/images/Main.java
+++ src/pl/uksw/images/Main.java
@@ -1,5 +1,6 @@
 package pl.uksw.images;
 
+import pl.uksw.images.section2.Section2;
 import pl.uksw.images.tiff.TiffFile;
 
 import javax.imageio.ImageIO;
@@ -14,14 +15,30 @@ public class Main {
         String inputFilePath = "C:\\Users\\aleksander\\IdeaProjects\\images\\resources\\file1.tif";
         String outputFilePath = "C:\\Users\\aleksander\\IdeaProjects\\images\\resources\\file2.tif";
 
+        String inputFilePathGrey = "C:\\Users\\aleksander\\IdeaProjects\\images\\resources\\fileGrey1.tif";
+        String outputFilePathGrey = "C:\\Users\\aleksander\\IdeaProjects\\images\\resources\\fileGrey2.tif";
+
         File inputFile =  new File(inputFilePath.replace("\\", "/"));
         File outputFile =  new File(outputFilePath.replace("\\", "/"));
 
+        File inputFileGrey =  new File(inputFilePathGrey.replace("\\", "/"));
+        File outputFileGrey =  new File(outputFilePathGrey.replace("\\", "/"));
+
         // Info, jak otwierać i zapisywać pliki
         BufferedImage testImage;
-        TiffFile test=new TiffFile();
-        testImage =test.readFile(inputFile);
+        TiffFile test = new TiffFile();
+        testImage = test.readFile(inputFile);
         test.saveFile(outputFile,testImage);
+
+        //zad 2
+        BufferedImage inzad2;
+        TiffFile zad2 = new TiffFile();
+        inzad2 = zad2.readFile(inputFileGrey);
+
+        Section2 sec2= new Section2();
+        sec2.sumPar(inzad2,150);
+
+        zad2.saveFile(outputFileGrey,inzad2);
 
     }
 }

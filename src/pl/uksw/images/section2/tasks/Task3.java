05359546d9e10f41884f8ef9b5a9c5e653063349
aleksanderkol00 <a.kolowski@student.uksw.edu.pl>
2018-05-17 20:45:57 +0200

    fix

--- src/pl/uksw/images/section2/tasks/Task3.java
+++ src/pl/uksw/images/section2/tasks/Task3.java
@@ -5,13 +5,5 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public class Task3 {
-
-    public BufferedImage task2(BufferedImage image, BufferedImage resized, double angle){
-
-        Graphics2D g = resized.createGraphics();
-        AffineTransform at = AffineTransform.getRotateInstance(angle);
-        g.drawRenderedImage(image, at);
-
-        return resized;
-    }
+    
 }
